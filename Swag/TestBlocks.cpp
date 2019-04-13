01afe399103f76d9c8241711c25d01a2a3ccb2b5
Nathan McIntyre <xdnathanm@gmail.com>
2019-04-13 11:06:07 -0600

    pincone

--- Swag/TestBlocks.cpp
+++ Swag/TestBlocks.cpp
@@ -1,12 +1,28 @@
 #include "pch.h"
 #include "TestBlocks.h"
-
+#include "allegro5/allegro_primitives.h"
 
 TestBlocks::TestBlocks()
 {
 }
 
-
 TestBlocks::~TestBlocks()
 {
 }
+void TestBlocks::update()
+{
+
+}
+void TestBlocks::draw()
+{
+	ALLEGRO_COLOR colorRed = al_map_rgb(255, 0, 0);
+
+
+	al_draw_filled_rectangle(0 ,0 ,64, 64, colorRed);
+
+
+}
+
+
+
+
--- Swag/TestBlocks.h
+++ Swag/TestBlocks.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "State.h"
-class TestBlocks :
+class TestBlocks:
 	public State
 {
 public:
 	TestBlocks();
 	~TestBlocks();
+	float x;
+	float y;
+	float width;
+	float height;
+	float velX;
+	float velY;
+	virtual void update();
+	virtual void draw();
 };
-
